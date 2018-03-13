3a6e4ad6896c1a843cfbe17744339def24b55eca
KassabNova <CKassabAndre@gmail.com>
2018-03-12 20:41:34 -0600

    Poniendo mis funciones en otro archivo y agregando SQLite

---
+++ VetMax/util.h
@@ -0,0 +1,8 @@
+void menuTitulo(void);
+void menuCuerpoRegistro(void);
+void menuCuerpoAbrir(void);
+void menuCuerpoModificar(void);
+void menuCuerpoCerrar(void);
+void tiempoActual(void);
+void menuSelect(void);
+void Creditos(void);
